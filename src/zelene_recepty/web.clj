0ec0e9301885f010ee3169c01360e870e1fd0ce4
dajanka <dajana.stiberova@gmail.com>
2014-12-05 18:50:16 +0100

    added HTTP to url for facebook and twitter share

--- src/zelene_recepty/web.clj
+++ src/zelene_recepty/web.clj
@@ -141,11 +141,11 @@
                                         (lang {:sk "Zdroj:" :en "Origin:"})
                                         (lang {:sk "Zdieľaj na facebooku" :en "Share on facebook"})
                                         (format "http://www.facebook.com/sharer.php?u=%s"
-                                                (URLEncoder/encode (format "%s/home#/recipe/%s/lang/%s" server-name recipe-id (name lang))
+                                                (URLEncoder/encode (format "http://%s/home#/recipe/%s/lang/%s" server-name recipe-id (name lang))
                                                                    "UTF-8"))
                                         (lang {:sk "Zdieľaj na twittri" :en "Share on twitter"})
                                         (format "http://twitter.com/share?url=%s"
-                                                (URLEncoder/encode (format "%s/home#/recipe/%s/lang/%s" server-name recipe-id (name lang))
+                                                (URLEncoder/encode (format "http://%s/home#/recipe/%s/lang/%s" server-name recipe-id (name lang))
                                                                    "UTF-8")))}))
 
 
